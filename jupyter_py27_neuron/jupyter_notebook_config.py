6bd7d9ca28f1d53bcdbbc423c5cd8d8ba8e2609c
paulstey <paulstey@gmail.com>
2018-03-09 16:08:48 -0500

    new image for neuron notebooks

---
+++ jupyter_py27_neuron/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.password = u'sha1:54bdc9bd838a:7c8bdd812db95c0514adf7b66b506917c7e6f772'
+c.NotebookApp.open_browser = False
+
+# It is a good idea to set a known, fixed port for server access
+c.NotebookApp.port = 8888
---
+++ jupyter_py27_neuron/start_jupyter.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source activate jupyter27
+jupyter notebook --ip 0.0.0.0
